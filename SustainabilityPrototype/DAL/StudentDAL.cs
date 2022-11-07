3e5ed0414e3d45561c38318f2c4145d88f7eb600
Wen Yu Tan <s10203894@connect.np.edu.sg>
2022-11-07 12:33:16 +0800

    Craeted student model and Student DAL, but connection string not detected, needa resolve.

---
+++ SustainabilityPrototype/DAL/StudentDAL.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Data.SqlClient;
+using SustainabilityPrototype.Models;
+
+namespace SustainabilityPrototype.DAL
+{
+    public class StudentDAL
+    {
+        private IConfiguration Configuration { get; }
+        private SqlConnection conn;
+
+        //Constructor
+        public StudentDAL()
+        {
+            //Read ConnectionString from appsettings.json file
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString(
+            "ZZFashionConnectionString");
+
+            //Instantiate a SqlConnection object with the 
+            //Connection String read. 
+            conn = new SqlConnection(strConn);
+        }
+
+        //Get Student details
+        public List<Student> GetAllStudent()
+        {
+            //Create a SqlCommand object from connection object
+            SqlCommand cmd = conn.CreateCommand();
+            //Specify the SELECT SQL statement 
+            cmd.CommandText = @"SELECT * FROM Student ORDER BY StudentID";
+            //Open a database connection
+            conn.Open();
+            //Execute the SELECT SQL through a DataReader
+            SqlDataReader reader = cmd.ExecuteReader();
+            //Read all records until the end, save data into a staff list
+            List<Student> studentList = new List<Student>();
+            while (reader.Read())
+            {
+                studentList.Add(
+                    new Student
+                    {
+                        StudentId = reader.GetString(0),
+                        StudentName = reader.GetString(1),
+                        Gender = reader.GetString(2)[0],
+                        DOB = reader.GetDateTime(3),
+                        StudentPassword = reader.GetString(4),
+                        StudentEmailAddr = !reader.IsDBNull(5) ?               // if not null
+                                 reader.GetString(5) : (string)null
+                    }
+                );
+            }
+            //Close DataReader
+            reader.Close();
+            //Close the database connection
+            conn.Close();
+
+            return studentList;
+        }
+}
---
+++ SustainabilityPrototype/Models/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SustainabilityPrototype.Models
+{
+    public class Student
+    {
+        //Student ID
+        [Display(Name = "id")]
+        public string StudentId { get; set; }
+
+        //Student name
+        [Display(Name = "Student name")]
+        [Required(ErrorMessage = "Please enter name"), StringLength(50)]
+        public string StudentName { get; set; }
+
+        //Gender
+        [Display(Name = "Gender")]
+        public char Gender { get; set; }
+
+        //Date of birth
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Field can't be empty")]
+        public DateTime? DOB { get; set; }
+
+        //Password 
+        [Display(Name = "Student password")]
+        public string StudentPassword { get; set; }
+
+        //Email
+        [Display(Name = "Student email address")]
+        public string StudentEmailAddr { get; set; }
+
+    }
+}
