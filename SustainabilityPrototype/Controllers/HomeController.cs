25c3733169beb404dc5bbbab0d5e936ce0b30080
LowHongWei <s10203927@connect.np.edu.sg>
2022-11-07 12:21:16 +0800

    Routed Login Page from Index Page

--- SustainabilityPrototype/Controllers/HomeController.cs
+++ SustainabilityPrototype/Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace SustainabilityPrototype.Controllers
         {
             return View();
         }
-
+        public ActionResult Login()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
