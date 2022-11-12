6540c7a3df6c5efc9638c9728a093e98a60bfd4e
Wen Yu Tan <s10203894@connect.np.edu.sg>
2022-11-12 18:38:45 +0800

    Created canteen dal, model and canteen nav home page, still kinda meh, needa refine

---
+++ SustainabilityPrototype/Models/Canteen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SustainabilityPrototype.Models
+{
+    public class Canteen
+    {
+        //CanteenID
+        [Display(Name = "id")]
+        public int CanteenId { get; set; }
+
+        //Canteen Name
+        [Display(Name = "Canteen name")]
+        [Required(ErrorMessage = "Please enter name"), StringLength(50)]
+        public string CanteenName { get; set; }
+
+        //Location
+        [Display(Name = "Location")]
+        [Required(ErrorMessage = "Please location"), StringLength(50)]
+        public string Location { get; set; }
+    }
+}
